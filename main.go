21d415ab117dfbfd69c9c49af8b6d1e311400f78
fajarpratamaputra <fajarpratamap@gmail.com>
2020-06-10 15:13:23 +0700

    github api

--- main.go
+++ main.go
@@ -1,13 +1,111 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
-	"encoding/json"
 	"net/http"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+	"context"
 	"fmt"
+	"os"
+	"text/tabwriter"
+	"time"
 )
 
+type Package struct {
+	FullName      string
+	Description   string
+	StarsCount    int
+	ForksCount    int
+	LastUpdatedBy string
+}
+
+// Owner is the repository owner
+type Owner struct {
+	Login string
+}
+
+// Item is the single repository data structure
+type Item struct {
+	ID              int
+	Name            string
+	FullName        string `json:"full_name"`
+	Owner           Owner
+	Description     string
+	CreatedAt       string `json:"created_at"`
+	StargazersCount int    `json:"stargazers_count"`
+}
+
+// JSONData contains the GitHub API response
+type JSONData struct {
+	Count int `json:"total_count"`
+	Items []Item
+}
+
+func repostars(w http.ResponseWriter, r *http.Request) {
+	res, err := http.Get("https://api.github.com/search/repositories?q=stars:>=10000+language:go&sort=stars&order=desc")
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if res.StatusCode != http.StatusOK {
+		log.Fatal("Unexpected status code", res.StatusCode)
+	}
+	data := JSONData{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Repositories found: %d", data.Count)
+	const format = "%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Repository", "Stars", "Created at", "Description")
+	fmt.Fprintf(tw, format, "----------", "-----", "----------", "----------")
+	for _, i := range data.Items {
+		desc := i.Description
+		if len(desc) > 50 {
+			desc = string(desc[:50]) + "..."
+		}
+		t, err := time.Parse(time.RFC3339, i.CreatedAt)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(tw, format, i.FullName, i.StargazersCount, t.Year(), desc)
+	}
+	tw.Flush()
+}
+
+func commit(w http.ResponseWriter, r *http.Request) {
+	context := context.Background()
+	tokenService := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: "6f991111e808fc490b99e42fe491c8b2addc8cd9"},
+	)
+	tokenClient := oauth2.NewClient(context, tokenService)
+
+	client := github.NewClient(tokenClient)
+
+	repo, _, err := client.Repositories.Get(context, "fajarpratamaputra", "github-repo")
+
+	if err != nil {
+		fmt.Printf("Problem in getting repository information %v\n", err)
+		os.Exit(1)
+	}
+
+	pack := &Package{
+		FullName: *repo.FullName,
+		Description: *repo.Description,
+		ForksCount: *repo.ForksCount,
+		StarsCount: *repo.StargazersCount,
+	}
+
+	fmt.Printf("%+v\n", pack)
+}
 
 func repo(w http.ResponseWriter, r *http.Request) {
 	res, err := http.Get("https://api.github.com/users/fajarpratamaputra/repos?go&sort=stars&order=desc")
@@ -31,6 +129,8 @@ func repo(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/repo", repo)
+	http.HandleFunc("/commit", commit)
+	http.HandleFunc("/repostars", repostars)
 
     fmt.Println("starting web server at http://localhost:8080/")
     http.ListenAndServe(":8080", nil)	
